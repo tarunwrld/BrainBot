39b692dfca8b5fe0b2eda3d0f251f924baef9735
tarunwrld <154779714+tarunwrld@users.noreply.github.com>
2023-12-25 12:16:59 +0530

    app.py

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ def main():
 
             st.markdown(page_bg_img, unsafe_allow_html=True)
 
-            repo_id = "google/flan-t5-xxl" 
+            repo_id = "gmistralai/Mistral-7B-v0.1" 
             st.title("Chat with BrainBot AI")
             st.markdown(""":red[**The BrainBot Ai is in early stages, can generete short inaccurate responses**]""")
             question = st.text_input("Write Something Here: ")
