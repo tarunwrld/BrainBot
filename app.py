84e449430d5a809fc2176a203467de3bc610e013
Tarun <154779714+tarunwrld@users.noreply.github.com>
2024-01-06 13:10:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from PIL import Image
 import time
 import requests
 import streamlit as st
-import socket
+import public_ip as ip
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACETOKEN1"]
 API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACETOKEN2"]
@@ -36,8 +36,8 @@ st.set_page_config(
     )
 
 def main():
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
+    IP = ip.get()
+    print(IP)
     
     print("Uer Computer Name is:" + hostname)
     print("User Computer IP Address is:" + IPAddr)
